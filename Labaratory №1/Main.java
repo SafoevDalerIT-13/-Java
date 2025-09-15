bde30f3b358319dbfebf081e083904a1bdc2697f
Далер <dsafoev40@gmail.com>
2025-09-15 06:19:07 +0300

    Update Main.java

--- Labaratory №1/Main.java
+++ Labaratory №1/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        Labaratory lab = new Labaratory();
-        lab.menu();
-    }
+  public static void main(String[] args) {
+    Labaratory lab = new Labaratory();
+    lab.menu();
+  }
 }
