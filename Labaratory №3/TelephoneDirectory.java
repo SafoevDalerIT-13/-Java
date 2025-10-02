4f53d81227fb5e23e289196a65a5e9030d8c3263
Далер <dsafoev40@gmail.com>
2025-10-02 17:41:25 +0300

    Update TelephoneDirectory.java

--- Labaratory №3/TelephoneDirectory.java
+++ Labaratory №3/TelephoneDirectory.java
@@ -2,72 +2,100 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TelephoneDirectory {
-    // Поля
-    private Map<String,String> telephoneName;
-    private Map<String,String> nameTelephone;
+  // Поля
+  private Map<String,String> telephoneName;
+  private Map<String,String> nameTelephone;
 
-    // Геттеры и сеттеры
-    public Map<String,String> getTelephoneName() {
-        return this.telephoneName;
+  // Геттеры и сеттеры
+  public Map<String,String> getTelephoneName() {
+    return this.telephoneName;
+  }
+
+  public Map<String,String> getNameTelephone() {
+    return this.nameTelephone;
+  }
+
+  public void setTelephoneName(String telephone, String name) {
+    this.telephoneName.put(telephone, name);
+    this.nameTelephone.put(name, telephone);
+  }
+
+  // Конструкторы
+  public TelephoneDirectory() {
+    this.telephoneName = new HashMap<>();
+    this.nameTelephone = new HashMap<>();
+  }
+
+  // Метод toString
+  @Override
+  public String toString() {
+    if (telephoneName.isEmpty()) {
+      return "Телефонный справочник пуст";
     }
 
-    public Map<String,String> getNameTelephone() {
-        return this.nameTelephone;
+    String result = "Телефонный справочник\n";
+    for (Map.Entry<String, String> entry : telephoneName.entrySet()) {
+      result += "Номер телефона - " + entry.getKey() + " Имя - " + entry.getValue() + "\n";
+    }
+    return result;
+  }
+
+  // Метод добавления User
+  public String addNewUser(String telephone, String name) {
+    String oldPhone = null;
+
+    if (nameTelephone.containsKey(name)) {
+      oldPhone = nameTelephone.get(name);
+
+      telephoneName.remove(oldPhone);
+      nameTelephone.remove(name);
     }
 
-    public void setTelephoneName(String telephone, String name) {
-        this.telephoneName.put(telephone, name);
-        this.nameTelephone.put(name, telephone);
+    telephoneName.put(telephone, name);
+    nameTelephone.put(name, telephone);
+
+    return oldPhone;
+  }
+  // Метод удаления user
+  public void deleteUser(String name) {
+    if (this.nameTelephone.containsKey(name)) {
+      String phone = nameTelephone.get(name);
+      nameTelephone.remove(name);
+      telephoneName.remove(phone);
     }
+  }
 
-    // Конструкторы
-    public TelephoneDirectory() {
-        this.telephoneName = new HashMap<>();
-        this.nameTelephone = new HashMap<>();
+  // Метод получения данных
+  public String findUser(String name) {
+    return nameTelephone.get(name);
+  }
+
+  // Метод проверки наличия телефона или имени
+  public void checkExists(String key) {
+    boolean isPhone = telephoneName.containsKey(key);
+    boolean isName = nameTelephone.containsKey(key);
+
+    if (isPhone) {
+      System.out.println("Телефон '" + key + "' есть в списке. Имя: " + telephoneName.get(key));
+    } else if (isName) {
+      System.out.println("Имя '" + key + "' есть в списке. Телефон: " + nameTelephone.get(key));
+    } else {
+      System.out.println("'" + key + "' не найден ни как телефон, ни как имя");
     }
+  }
 
-    // Метод toString
-    @Override
-    public String toString() {
-        if (telephoneName.isEmpty()) {
-            return "Телефонный справочник пуст";
-        }
-
-        String result = "Телефонный справочник\n";
-        for (Map.Entry<String, String> entry : telephoneName.entrySet()) {
-            result += "Номер телефона - " + entry.getKey() + " Имя - " + entry.getValue() + "\n";
-        }
-        return result;
+  // Узнаем текущее кол-во контактов
+  // Узнаем текущее количество контактов
+  public void sizeMap() {
+    if (this.nameTelephone.isEmpty()) {
+      System.out.println("Список пуст!");
+    } else {
+      System.out.println("Количество контактов: " + this.nameTelephone.size());
     }
+  }
 
-    // Метод добавления User
-    public String addNewUser(String telephone, String name) {
-        String oldPhone = null;
 
-        if (nameTelephone.containsKey(name)) {
-            oldPhone = nameTelephone.get(name);
 
-            telephoneName.remove(oldPhone);
-            nameTelephone.remove(name);
-        }
 
-        telephoneName.put(telephone, name);
-        nameTelephone.put(name, telephone);
-
-        return oldPhone;
-    }
-    // Метод удаления user
-    public void deleteUser(String key) {
-        if (this.telephoneName.containsKey(key)) {
-            String name = telephoneName.get(key);
-            telephoneName.remove(key);
-            nameTelephone.remove(name);
-        }
-        if (this.nameTelephone.containsKey(key)) {
-            String name1 = nameTelephone.get(key);
-            nameTelephone.remove(key);
-            telephoneName.remove(name1);
-        }
-    }
 
 }
