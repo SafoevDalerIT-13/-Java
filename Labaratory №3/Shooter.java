08d1bf09b3fe87c32f431e4cc97af4372eb56628
Далер <dsafoev40@gmail.com>
2025-10-05 17:30:13 +0500

    Create Shooter.java

---
+++ Labaratory №3/Shooter.java
@@ -0,0 +1,58 @@
+public class Shooter {
+    // Поля
+    private String name;
+    private Weapon weapon;
+
+    //Геттеры и сеттеры
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Weapon getWeapon() {
+        return this.weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    //Конструкторы
+    public Shooter(String name) {
+        this.name = name;
+        this.weapon = null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    public Shooter(String name, Weapon weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+
+    //Метод toString
+    @Override
+    public String toString() {
+        return "Вы создали Стрелка с именем - " + getName();
+    }
+
+    // Основная способность - стрелять
+    public void shoot() {
+        if(weapon != null) {
+            System.out.print(name + " стреляет: ");
+            weapon.shoot();
+        } else {
+            System.out.println(name + ": не могу участвовать в перестрелке");
+        }
+    }
+
+
+
+
+}
