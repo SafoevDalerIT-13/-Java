f3958d4509dd30f46072b12b47c4dd30e319ce6c
Далер <dsafoev40@gmail.com>
2025-10-12 15:28:52 +0500

    Delete Labaratory №3/ru.safoev/application/Main.java

--- Labaratory №3/ru.safoev/application/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.safoev.application;
-
-public class Main {
-    public static void main(String[] args) {
-        MenuAndValidator menu = new MenuAndValidator();
-        menu.menu();
-    }
-}
