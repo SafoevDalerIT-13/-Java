adc4130e9f09af2363ccd67b092f57ff4b3a35fd
Далер <dsafoev40@gmail.com>
2025-10-24 18:43:58 +0500

    Create Main.java

---
+++ Labaratory №4/ru.safoev/main/Main.java
@@ -0,0 +1,8 @@
+package ru.safoev.main;
+
+public class Main {
+    public static void main(String[] args) {
+        ru.safoev.menu.MenuAndValidator menuAndValidator = new ru.safoev.menu.MenuAndValidator();
+        menuAndValidator.menu();
+    }
+}
