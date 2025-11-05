9c9499883b4ed3bd7702c2de7315ee2cc7a01a9e
Далер <dsafoev40@gmail.com>
2025-11-05 16:06:58 +0300

    Update Main.java

--- Labaratory №5/ru.safoev/main/Main.java
+++ Labaratory №5/ru.safoev/main/Main.java
@@ -1,1 +1,11 @@
 
+package ru.safoev.main;
+
+import ru.safoev.menu.MenuAndValidator;
+
+public class Main {
+  public static void main(String[] args) {
+    MenuAndValidator menuAndValidator = new MenuAndValidator();
+    menuAndValidator.menu();
+  }
+}
