4c4ff6f2478b1a81bbbfd01ea016913436ce27e6
Далер <dsafoev40@gmail.com>
2025-11-09 14:41:39 +0300

    Create Funs.java

---
+++ Labaratory №5/ru.safoev/cats/Funs.java
@@ -0,0 +1,10 @@
+package ru.safoev.cats;
+public class Funs {
+  public static void meowsCare(Meowable meowable) {
+    meowable.meow();
+    meowable.meow();
+    meowable.meow();
+    meowable.meow();
+    meowable.meow();
+  }
+}
