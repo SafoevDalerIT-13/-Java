9fd80bbd1caf8b045d2a278f4550d0b909de2db3
Далер <dsafoev40@gmail.com>
2025-11-11 14:38:46 +0500

    Create DefaultHandler.java

---
+++ Labaratory №6/ru.safoev/defaultPackage/DefaultHandler.java
@@ -0,0 +1,27 @@
+package ru.safoev.defaultPackage;
+
+import java.lang.reflect.Field;
+
+public class DefaultHandler {
+    public static void DefaultStart() {
+        TestDefault testDefault = new TestDefault();
+        Class<?> classDefault = testDefault.getClass();
+
+        System.out.println("Имя класса: " + classDefault.getSimpleName());
+
+        if (classDefault.isAnnotationPresent(Default.class)) {
+            Default annotation = classDefault.getAnnotation(Default.class);
+            Class<?> defaultClass = annotation.value();
+            System.out.println("Класс имеет аннотацию @Default с типом: " + defaultClass.getSimpleName());
+        } else {
+            System.out.println("Класс не имеет аннотации @Default на типе");
+        }
+
+        Field[] fields = classDefault.getDeclaredFields();
+        for (Field field : fields) {
+            if(field.isAnnotationPresent(Default.class)) {
+                System.out.println("Поле " + "\"" + field.getName() + "\"" + " имеет аннотацию @Default");
+            }
+        }
+    }
+}
