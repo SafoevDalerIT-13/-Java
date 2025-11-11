928dd773a5002144a6b71f92406eb8bb5ec9dedc
Далер <dsafoev40@gmail.com>
2025-11-11 14:39:56 +0500

    Create Main.java

---
+++ Labaratory №6/ru.safoev/mainPackage/Main.java
@@ -0,0 +1,14 @@
+package ru.safoev.mainPackage;
+
+import ru.safoev.defaultPackage.DefaultHandler;
+import ru.safoev.invokePackage.InvokeHandler;
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        // Категория 1: Задание 1
+        InvokeHandler.InvokeStart();
+        // Категория 1: Задание 2
+        DefaultHandler.DefaultStart();
+  }
+}
