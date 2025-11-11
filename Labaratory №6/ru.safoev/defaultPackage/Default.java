98355e289f2cf3c7f1f26aa56c60e0314db89836
Далер <dsafoev40@gmail.com>
2025-11-11 14:38:30 +0500

    Update Default.java

--- Labaratory №6/ru.safoev/defaultPackage/Default.java
+++ Labaratory №6/ru.safoev/defaultPackage/Default.java
@@ -1,1 +1,12 @@
+package ru.safoev.defaultPackage;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Default {
+    Class<?> value();
+}
