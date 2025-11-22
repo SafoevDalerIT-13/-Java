796caa513946ecfe87eb24011047ef34ee8208b5
Далер <dsafoev40@gmail.com>
2025-11-22 17:52:17 +0300

    Update Validate.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/validatePackage/Validate.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/validatePackage/Validate.java
@@ -5,9 +5,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для валидации классов, указывающая список классов,
+ * которые должны быть проверены при валидации аннотированного класса.
+ * Может применяться как к классам, так и к другим аннотациям.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 @Target({ElementType.TYPE,ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Validate {
-    Class<?>[] value();
 
+  /**
+   * Массив классов для валидации.
+   * Определяет список классов, которые должны быть проверены
+   * при обработке аннотированного элемента.
+   *
+   * @return массив классов для валидации
+   */
+  Class<?>[] value();
 }
