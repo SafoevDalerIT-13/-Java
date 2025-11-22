cc56d2365621daab87d4d9362036178740c41c9e
Далер <dsafoev40@gmail.com>
2025-11-22 17:53:26 +0300

    Update InvokeHandlerTest.java

--- Labaratory №6/src/test/java/ru.safoev/invokePackage/InvokeHandlerTest.java
+++ Labaratory №6/src/test/java/ru.safoev/invokePackage/InvokeHandlerTest.java
@@ -7,37 +7,57 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-
+/**
+ * Тестовый класс для проверки работы аннотации @Invoke и обработчика InvokeHandler.
+ * Содержит тесты для проверки корректного вызова методов с аннотацией @Invoke
+ * и проверки изменений состояния объекта после вызова методов.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 public class InvokeHandlerTest {
 
+  /**
+   * Тестовый объект для проверки работы аннотации @Invoke.
+   */
   private TestInvoke testInvoke;
 
+  /**
+   * Метод, выполняемый перед каждым тестом.
+   * Создает новый экземпляр TestInvoke для изоляции тестов.
+   */
   @BeforeEach
   public void createObject() {
     testInvoke = new TestInvoke();
   }
 
+  /**
+   * Комплексный тест для проверки работы аннотации @Invoke.
+   * Проверяет корректный вызов всех методов с аннотацией @Invoke,
+   * возвращаемые значения методов и изменения состояния объекта.
+   */
   @Test
   public void test1() {
-    //Тут Ref API
+    // Получение методов через Reflection API
     Class<?> invokeClass = testInvoke.getClass();
     Method[] methods = invokeClass.getDeclaredMethods();
     int initialNum = testInvoke.getNum();
+
+    // Проверка вызова всех методов с аннотацией @Invoke без исключений
     for (Method method : methods) {
-      //Тут проверка без исключ
       if(method.isAnnotationPresent(Invoke.class)) {
         Assertions.assertDoesNotThrow(() -> {
           method.setAccessible(true);
           method.invoke(testInvoke);
-       });
-
+        });
       }
     }
-    //Тут отдельно сделал прверку метода на штуки
-    Assertions.assertEquals(232,  testInvoke.Test2());
 
+    // Проверка возвращаемого значения метода Test2
+    Assertions.assertEquals(232, testInvoke.Test2());
+
+    // Проверка изменения состояния объекта после вызова методов
     Assertions.assertEquals(initialNum + 1, testInvoke.getNum());
-
   }
-
 }
