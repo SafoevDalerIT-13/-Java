a225e494906185942261eb90ee1975208e00f5f4
Далер <dsafoev40@gmail.com>
2025-11-22 17:48:42 +0300

    Update InvokeHandler.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/invokePackage/InvokeHandler.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/invokePackage/InvokeHandler.java
@@ -3,19 +3,36 @@ package ru.safoev.invokePackage;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+/**
+ * Обработчик для вызова методов с аннотацией @Invoke.
+ * Использует механизм рефлексии для автоматического вызова всех методов,
+ * помеченных аннотацией @Invoke у переданного объекта.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 public class InvokeHandler {
-    public static void InvokeStart(Object object) throws InvocationTargetException, IllegalAccessException {
-        Class<?> classInvoke = object.getClass();
 
-        Method[] methods = classInvoke.getDeclaredMethods();
-        for (Method method : methods) {
-            if(method.isAnnotationPresent(Invoke.class)) {
-                method.setAccessible(true);
-                System.out.print("Метод " + "\"" + method.getName() + "\""  + " (модификатор доступа: " + method.getModifiers() + ")" + " имеет аннотацию @Invoke, поэтому вызываем его: ");
-                method.invoke(object);
-                method.setAccessible(false);
-            }
-        }
+  /**
+   * Вызывает все методы с аннотацией @Invoke у переданного объекта.
+   * Методы вызываются независимо от их модификатора доступа (public, private, protected).
+   *
+   * @param object объект, методы которого нужно вызвать.
+   * @throws InvocationTargetException если произошла ошибка при вызове метода через рефлексию.
+   * @throws IllegalAccessException если нет доступа к методу через рефлексию.
+   */
+  public static void InvokeStart(Object object) throws InvocationTargetException, IllegalAccessException {
+    Class<?> classInvoke = object.getClass();
 
+    Method[] methods = classInvoke.getDeclaredMethods();
+    for (Method method : methods) {
+      if(method.isAnnotationPresent(Invoke.class)) {
+        method.setAccessible(true);
+        System.out.print("Метод " + "\"" + method.getName() + "\""  + " (модификатор доступа: " + method.getModifiers() + ")" + " имеет аннотацию @Invoke, поэтому вызываем его: ");
+        method.invoke(object);
+        method.setAccessible(false);
+      }
     }
+  }
 }
