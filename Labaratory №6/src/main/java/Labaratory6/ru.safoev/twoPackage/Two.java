8b0b7ae3ac9cb537373d31768159b815b5b60901
Далер <dsafoev40@gmail.com>
2025-11-22 17:50:57 +0300

    Update Two.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/twoPackage/Two.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/twoPackage/Two.java
@@ -5,9 +5,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация с двумя свойствами разных типов для маркировки классов.
+ * Демонстрирует использование аннотаций с обязательными параметрами.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Two {
+
+  /**
+   * Строковое свойство аннотации.
+   * Обязательный параметр без значения по умолчанию.
+   *
+   * @return строковое значение свойства first
+   */
   String first();
+
+  /**
+   * Числовое свойство аннотации.
+   * Обязательный параметр без значения по умолчанию.
+   *
+   * @return целочисленное значение свойства second
+   */
   int second();
 }
