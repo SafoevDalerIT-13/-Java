a9d312658c5868fd14466828aa6cc0a65db81339
Далер <dsafoev40@gmail.com>
2025-11-22 17:47:40 +0300

    Update Default.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/defaultPackage/Default.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/defaultPackage/Default.java
@@ -5,8 +5,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для указания классов и полей по умолчанию.
+ * Может применяться как к классам, так и к отдельным полям.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 @Target({ElementType.FIELD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Default {
-    Class<?> value();
+  /**
+   * Определяет класс по умолчанию для аннотированного элемента.
+   *
+   * @return класс по умолчанию для аннотированного элемента.
+   */
+  Class<?> value();
 }
