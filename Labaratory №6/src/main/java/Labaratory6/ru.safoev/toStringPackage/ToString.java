fc06a20a99db228c2aa68be605918321484faa76
Далер <dsafoev40@gmail.com>
2025-11-22 17:50:17 +0300

    Update ToString.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/toStringPackage/ToString.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/toStringPackage/ToString.java
@@ -5,11 +5,35 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для кастомного строкового представления объектов.
+ * Позволяет управлять включением полей в строковое представление
+ * как на уровне класса, так и на уровне отдельных полей.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 @Target({ElementType.TYPE, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ToString {
-    enum Mode{ YES, NO }
 
-    Mode value() default Mode.YES;
+  /**
+   * Перечисление режимов отображения полей в строковом представлении.
+   */
+  enum Mode{
+    /** Включать поле в строковое представление */
+    YES,
+    /** Исключать поле из строкового представления */
+    NO
+  }
 
+  /**
+   * Определяет режим отображения для аннотированного элемента.
+   * Для класса устанавливает режим по умолчанию для всех полей.
+   * Для поля переопределяет режим, установленный на уровне класса.
+   *
+   * @return режим отображения элемента (по умолчанию YES)
+   */
+  Mode value() default Mode.YES;
 }
