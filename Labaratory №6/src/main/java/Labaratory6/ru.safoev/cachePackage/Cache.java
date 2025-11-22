9189cf3db7b4917c6c6518892dcc71143e167b46
Далер <dsafoev40@gmail.com>
2025-11-22 17:46:40 +0300

    Update Cache.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/cachePackage/Cache.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/cachePackage/Cache.java
@@ -5,9 +5,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для маркировки классов, указывающая кешируемые области.
+ * Позволяет определить список областей, которые должны кешироваться
+ * для аннотированного класса.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Cache {
-  String[] value() default {};
 
+  /**
+   * Массив строк с названиями кешируемых областей.
+   * Каждая строка представляет отдельную область кеширования.
+   *
+   * @return массив названий областей кеширования (по умолчанию пустой массив)
+   */
+  String[] value() default {};
 }
