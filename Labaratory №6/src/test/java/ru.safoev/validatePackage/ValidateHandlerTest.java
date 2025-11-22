85f4248f17dff9d3859be09ffaa5bc08e5e744be
Далер <dsafoev40@gmail.com>
2025-11-22 16:35:48 +0300

    Update ValidateHandlerTest.java

--- Labaratory №6/src/test/java/ru.safoev/validatePackage/ValidateHandlerTest.java
+++ Labaratory №6/src/test/java/ru.safoev/validatePackage/ValidateHandlerTest.java
@@ -5,10 +5,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidateHandlerTest {
+
+  @Test
+  void CorrectAnnotationTestMB() {
+    Validate validate = ValidateTest.class.getAnnotation(Validate.class);
+    Assertions.assertNotNull(validate);
+    Class<?>[] validationClasses = validate.value();
+
+    System.out.println("Класс " + ValidateTest.class.getSimpleName() + " имеет аннотацию @Validate");
+    System.out.println("Количество классов для проверки: " + validationClasses.length);
+    System.out.println("Список классов для проверки:");
+
+    for (int i = 0; i < validationClasses.length; i++) {
+      Class<?> currentClass = validationClasses[i];
+      System.out.println((i + 1) + ". " + currentClass.getName() +
+              " (простое имя: " + currentClass.getSimpleName() + ")");
+    }
+
+  }
   @Test
   void CorrectAnnotationTest() {
     Validate validate = ValidateTest.class.getAnnotation(Validate.class);
     Assertions.assertNotNull(validate);
+
 
     Class<?>[] validationClasses = validate.value();
     Assertions.assertEquals(2, validationClasses.length);
