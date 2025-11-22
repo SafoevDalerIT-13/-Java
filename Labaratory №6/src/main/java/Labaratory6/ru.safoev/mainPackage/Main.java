3e76a62fe66a237bd30c7794f432ecfc905d21ae
Далер <dsafoev40@gmail.com>
2025-11-22 17:49:14 +0300

    Update Main.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/mainPackage/Main.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/mainPackage/Main.java
@@ -2,7 +2,22 @@ package ru.safoev.mainPackage;
 
 import ru.safoev.menuPackage.MenuAndValidator;
 
+/**
+ * Главный класс приложения, содержащий точку входа в программу.
+ * Инициализирует и запускает систему меню для тестирования функциональности аннотаций.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 public class Main {
+
+  /**
+   * Точка входа в приложение. Создает экземпляр класса MenuAndValidator
+   * и запускает основное меню программы.
+   *
+   * @param args аргументы командной строки (не используются).
+   */
   public static void main(String[] args) {
     MenuAndValidator menu = new MenuAndValidator();
     menu.menu();
