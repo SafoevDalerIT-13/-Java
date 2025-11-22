85132982b252065b6c4fd749758302770f6e8065
Далер <dsafoev40@gmail.com>
2025-11-22 17:52:31 +0300

    Update ValidateHandler.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/validatePackage/ValidateHandler.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/validatePackage/ValidateHandler.java
@@ -1,15 +1,38 @@
 package ru.safoev.validatePackage;
 
+/**
+ * Обработчик аннотации @Validate для анализа классов и извлечения информации
+ * о классах валидации. Предоставляет функциональность для проверки
+ * наличия аннотации @Validate и вывода списка классов для валидации.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 public class ValidateHandler {
 
+  /**
+   * Анализирует класс на наличие аннотации @Validate и выводит информацию
+   * о классах для валидации. Проверяет, что массив классов не пустой,
+   * и выводит подробную информацию о каждом классе.
+   *
+   * @param classes класс для анализа на наличие аннотации @Validate
+   * @throws IllegalArgumentException если массив классов для валидации пустой
+   */
   public static void ValidateStart(Class<?> classes) {
     if (classes.isAnnotationPresent(Validate.class)) {
       Validate validateAnnotation = classes.getAnnotation(Validate.class);
       Class<?>[] validationClasses = validateAnnotation.value();
 
+      // Проверка на пустой массив классов
+      if(validationClasses.length == 0) {
+        throw new IllegalArgumentException("Массив классов для валидации не может быть пустым!");
+      }
+
       System.out.println("Класс " + classes.getSimpleName() + " помечен @Validate");
       System.out.println("Список классов для проверки:");
 
+      // Вывод информации о каждом классе валидации
       for (int i = 0; i < validationClasses.length; i++) {
         Class<?> validationClass = validationClasses[i];
         System.out.println((i + 1) + ". " + validationClass.getName() +
