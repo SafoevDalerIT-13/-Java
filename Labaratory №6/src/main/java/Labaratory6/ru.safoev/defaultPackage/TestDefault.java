4998e459887da8a66cfa28f62b55950665738aa4
Далер <dsafoev40@gmail.com>
2025-11-22 17:48:14 +0300

    Update TestDefault.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/defaultPackage/TestDefault.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/defaultPackage/TestDefault.java
@@ -1,31 +1,65 @@
 package ru.safoev.defaultPackage;
 
+/**
+ * Тестовый класс для демонстрации работы аннотации @Default.
+ * Класс аннотирован на уровне типа и содержит поле с аннотацией.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 @Default(String.class)
 public class TestDefault {
 
-    @Default(String.class)
-    private String name;
+  /**
+   * Поле name с аннотацией @Default, указывающей на строковый тип по умолчанию.
+   */
+  @Default(String.class)
+  private String name;
 
-    public TestDefault() {
-        this.name = null;
-    }
+  /**
+   * Конструктор по умолчанию, инициализирует поле name значением null.
+   */
+  public TestDefault() {
+    this.name = null;
+  }
 
-    public TestDefault(String name) {
-        this.name = name;
-    }
+  /**
+   * Конструктор с параметром для инициализации поля name.
+   *
+   * @param name значение для инициализации поля name.
+   */
+  public TestDefault(String name) {
+    this.name = name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  /**
+   * Возвращает значение поля name.
+   *
+   * @return текущее значение поля name.
+   */
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  /**
+   * Устанавливает новое значение для поля name.
+   *
+   * @param name новое значение для поля name.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    @Override
-    public String toString() {
-        return "TestDefault{" +
-                "name='" + name + '\'' +
-                '}';
-    }
+  /**
+   * Возвращает строковое представление объекта TestDefault.
+   *
+   * @return строковое представление объекта в формате "TestDefault{name='значение'}".
+   */
+  @Override
+  public String toString() {
+    return "TestDefault{" +
+            "name='" + name + '\'' +
+            '}';
+  }
 }
