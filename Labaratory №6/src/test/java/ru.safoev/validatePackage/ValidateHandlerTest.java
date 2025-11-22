6cf3c6808df8bf0d136c5aa08205fd2d03beb875
Далер <dsafoev40@gmail.com>
2025-11-22 17:53:47 +0300

    Update ValidateHandlerTest.java

--- Labaratory №6/src/test/java/ru.safoev/validatePackage/ValidateHandlerTest.java
+++ Labaratory №6/src/test/java/ru.safoev/validatePackage/ValidateHandlerTest.java
@@ -4,8 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Тестовый класс для проверки работы аннотации @Validate и обработчика ValidateHandler.
+ * Содержит тесты для проверки корректности аннотации, извлечения значений
+ * и обработки исключительных ситуаций.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 class ValidateHandlerTest {
 
+  /**
+   * Тест для проверки корректности аннотации @Validate с выводом подробной информации.
+   * Проверяет наличие аннотации и выводит детальную информацию о классах валидации
+   * в консоль для отладки и демонстрации.
+   */
   @Test
   void CorrectAnnotationTestMB() {
     Validate validate = ValidateTest.class.getAnnotation(Validate.class);
@@ -21,13 +35,17 @@ class ValidateHandlerTest {
       System.out.println((i + 1) + ". " + currentClass.getName() +
               " (простое имя: " + currentClass.getSimpleName() + ")");
     }
-
   }
+
+  /**
+   * Тест для проверки корректности аннотации @Validate.
+   * Проверяет наличие аннотации, количество классов валидации
+   * и соответствие ожидаемым типам классов.
+   */
   @Test
   void CorrectAnnotationTest() {
     Validate validate = ValidateTest.class.getAnnotation(Validate.class);
     Assertions.assertNotNull(validate);
-
 
     Class<?>[] validationClasses = validate.value();
     Assertions.assertEquals(2, validationClasses.length);
@@ -35,6 +53,11 @@ class ValidateHandlerTest {
     Assertions.assertEquals(Integer.class, validationClasses[1]);
   }
 
+  /**
+   * Тест для проверки обработки исключительной ситуации при пустом массиве классов.
+   * Проверяет, что при вызове ValidateHandler с классом, имеющим пустой массив
+   * классов валидации, выбрасывается исключение с корректным сообщением.
+   */
   @Test
   void ExceptionTest() {
     IllegalArgumentException exception = Assertions.assertThrows(
