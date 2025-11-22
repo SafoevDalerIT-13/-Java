7f5dd2cb314a43a5957eb4519305df5d7a98c365
Далер <dsafoev40@gmail.com>
2025-11-22 17:52:05 +0300

    Create EmptyValidateTest.java

---
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/validatePackage/EmptyValidateTest.java
@@ -0,0 +1,93 @@
+package ru.safoev.validatePackage;
+
+/**
+ * Тестовый класс для демонстрации обработки пустого массива классов
+ * в аннотации @Validate. Используется для тестирования исключительных
+ * ситуаций при валидации.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
+@Validate({})
+public class EmptyValidateTest {
+
+  /**
+   * Заголовок тестового объекта.
+   */
+  private String title;
+
+  /**
+   * Продолжительность тестового объекта.
+   */
+  private int duration;
+
+  /**
+   * Конструктор по умолчанию.
+   * Инициализирует поля значениями по умолчанию.
+   */
+  public EmptyValidateTest() {
+    this.title = null;
+    this.duration = 0;
+  }
+
+  /**
+   * Конструктор с параметрами для инициализации всех полей объекта.
+   *
+   * @param title заголовок тестового объекта
+   * @param duration продолжительность тестового объекта
+   */
+  public EmptyValidateTest(String title, int duration) {
+    this.title = title;
+    this.duration = duration;
+  }
+
+  /**
+   * Возвращает заголовок тестового объекта.
+   *
+   * @return текущий заголовок тестового объекта
+   */
+  public String getTitle() {
+    return title;
+  }
+
+  /**
+   * Устанавливает новый заголовок тестового объекта.
+   *
+   * @param title новый заголовок тестового объекта
+   */
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  /**
+   * Возвращает продолжительность тестового объекта.
+   *
+   * @return текущая продолжительность тестового объекта
+   */
+  public int getDuration() {
+    return duration;
+  }
+
+  /**
+   * Устанавливает новую продолжительность тестового объекта.
+   *
+   * @param duration новая продолжительность тестового объекта
+   */
+  public void setDuration(int duration) {
+    this.duration = duration;
+  }
+
+  /**
+   * Возвращает строковое представление объекта EmptyValidateTest.
+   *
+   * @return строковое представление объекта в формате "Test1{title='значение', duration=число}"
+   */
+  @Override
+  public String toString() {
+    return "Test1{" +
+            "title='" + title + '\'' +
+            ", duration=" + duration +
+            '}';
+  }
+}
