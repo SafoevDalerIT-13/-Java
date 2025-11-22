08f1b030eb7988e8daa3ea439216ddfc7458e9ff
Далер <dsafoev40@gmail.com>
2025-11-22 16:37:04 +0300

    Update TestInvoke.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/invokePackage/TestInvoke.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/invokePackage/TestInvoke.java
@@ -1,45 +1,52 @@
 package ru.safoev.invokePackage;
 
 public class TestInvoke {
-    
-    @Invoke
-    public void Test1() {
-        System.out.println("Вызвали public void тест под номером один!");
-    }
+  private int num = 1;
 
-    @Invoke
-    public void Test2() {
-        System.out.println("Вызвали public void тест под номером два!");
-    }
+  @Invoke
+  public void Test1() {
+    System.out.println("Вызвали public void тест под номером один!");
+    this.num = 2;
+  }
 
-    @Invoke
-    public void Test3() {
-        System.out.println("Вызвали public void тест под номером три!");
-    }
+  @Invoke
+  public int Test2() {
+    System.out.println("Вызвали public void тест под номером два!");
+    return 232;
+  }
 
-    @Invoke
-    public static void Test4() {
-        System.out.println("Вызвали public static void тест под номером четыре!");
-    }
+  @Invoke
+  public void Test3() {
+    System.out.println("Вызвали public void тест под номером три!");
+  }
 
-    @Invoke
-    private void Test5() {
-        System.out.println("Вызвали private void тест под номером пять!");
-    }
+  @Invoke
+  public static void Test4() {
+    System.out.println("Вызвали public static void тест под номером четыре!");
+  }
 
-    @Invoke
-    private static void Test6() {
-        System.out.println("Вызвали private static void тест под номером шесть!");
-    }
+  @Invoke
+  private void Test5() {
+    System.out.println("Вызвали private void тест под номером пять!");
+  }
 
-    @Invoke
-    protected void Test7() {
-        System.out.println("Вызвали protected void тест под номером семь!");
-    }
+  @Invoke
+  private static void Test6() {
+    System.out.println("Вызвали private static void тест под номером шесть!");
+  }
 
-    @Invoke
-    protected static void Test8() {
-        System.out.println("Вызвали protected static void тест под номером восемь!");
-    }
-    
+  @Invoke
+  protected void Test7() {
+    System.out.println("Вызвали protected void тест под номером семь!");
+  }
+
+  @Invoke
+  protected static void Test8() {
+    System.out.println("Вызвали protected static void тест под номером восемь!");
+  }
+
+  public int getNum() {
+    return this.num;
+  }
+
 }
