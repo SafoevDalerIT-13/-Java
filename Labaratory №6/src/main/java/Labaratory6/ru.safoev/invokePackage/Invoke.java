2fc27f52a53b47fa0e2c6e9d69d2916ac198e1d7
Далер <dsafoev40@gmail.com>
2025-11-22 17:48:29 +0300

    Update Invoke.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/invokePackage/Invoke.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/invokePackage/Invoke.java
@@ -5,6 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для автоматического вызова методов.
+ * Методы, помеченные этой аннотацией, могут быть автоматически
+ * вызваны через механизм рефлексии.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Invoke {
