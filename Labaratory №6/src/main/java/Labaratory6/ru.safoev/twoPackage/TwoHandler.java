307e4373f2c3b35229aff2381c17ae14ff9a0a83
Далер <dsafoev40@gmail.com>
2025-11-22 17:51:11 +0300

    Update TwoHandler.java

--- Labaratory №6/src/main/java/Labaratory6/ru.safoev/twoPackage/TwoHandler.java
+++ Labaratory №6/src/main/java/Labaratory6/ru.safoev/twoPackage/TwoHandler.java
@@ -1,6 +1,23 @@
 package ru.safoev.twoPackage;
 
+/**
+ * Обработчик аннотации @Two для анализа классов и извлечения значений свойств аннотации.
+ * Предоставляет функциональность для проверки наличия аннотации @Two
+ * и вывода информации о её свойствах.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
 public class TwoHandler {
+
+  /**
+   * Анализирует класс на наличие аннотации @Two и выводит информацию о её свойствах.
+   * Если аннотация присутствует, извлекает значения свойств first и second
+   * и выводит их в форматированном виде.
+   *
+   * @param class1 класс для анализа на наличие аннотации @Two
+   */
   public static void twoStart(Class<?> class1) {
     if(class1.isAnnotationPresent(Two.class)) {
       Two two = class1.getAnnotation(Two.class);
@@ -14,4 +31,3 @@ public class TwoHandler {
     }
   }
 }
-
